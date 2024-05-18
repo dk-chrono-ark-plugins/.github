2caf300977436a33ad8211cfd4070c1e73e09e4e
DK <35783662+gottyduke@users.noreply.github.com>
2024-05-18 08:30:58 -0500

    chore: update miscs

--- Common/Helper/DeferredCoroutine.cs
+++ Common/Helper/DeferredCoroutine.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 namespace ChronoArkMod.Helper;
 
-internal static class DelayedCoroutine
+internal static class DeferredCoroutine
 {
     internal static Coroutine StartDeferredCoroutine(this MonoBehaviour instance, Action action, int frames = 1)
     {
         return instance.StartCoroutine(DeferredFrames(action, frames));
+    }
+
+    internal static Coroutine StartDeferredCoroutine(this MonoBehaviour instance, Action action, float seconds)
+    {
+        return instance.StartCoroutine(DeferredSeconds(action, seconds));
     }
 
     internal static Coroutine StartDeferredCoroutine(this MonoBehaviour instance, Action action, Func<bool> predicate)
@@ -21,6 +26,12 @@ internal static class DelayedCoroutine
         for (int i = 0; i < frames; ++i) {
             yield return null;
         }
+        action();
+    }
+
+    private static IEnumerator DeferredSeconds(Action action, float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
         action();
     }
 
