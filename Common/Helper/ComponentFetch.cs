01f0026768ab523740db87a7e84dd7e3dbf03109
DK <35783662+gottyduke@users.noreply.github.com>
2024-05-18 02:55:31 -0500

    build: init distributed helpers

---
+++ Common/Helper/ComponentFetch.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace ChronoArkMod.Helper;
+
+internal static class ComponentFetch
+{
+    public static T GetOrAddComponent<T>(this GameObject gameObject) where T : Component
+    {
+        return gameObject.GetComponent<T>() ?? gameObject.AddComponent<T>();
+    }
+
+    public static bool TryGetComponent<T>(this GameObject gameObject, out T component) where T : Component
+    {
+        component = gameObject.GetComponent<T>();
+        return component != null;
+    }
+
+    public static bool TryGetComponentInParent<T>(this GameObject gameObject, out T component) where T : Component
+    {
+        component = gameObject.GetComponentInParent<T>();
+        return component != null;
+    }
+
+    public static bool TryGetComponentInChildren<T>(this GameObject gameObject, out T component) where T : Component
+    {
+        component = gameObject.GetComponentInChildren<T>();
+        return component != null;
+    }
+}
---
+++ Common/Helper/DelayedCoroutine.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace ChronoArkMod.Helper;
+
+internal static class DelayedCoroutine
+{
+    internal static Coroutine StartDeferredCoroutine(this MonoBehaviour instance, Action action, int frames = 1)
+    {
+        return instance.StartCoroutine(DeferredFrames(action, frames));
+    }
+
+    internal static Coroutine StartDeferredCoroutine(this MonoBehaviour instance, Action action, Func<bool> predicate)
+    {
+        return instance.StartCoroutine(DeferredAwaiter(action, predicate));
+    }
+
+    private static IEnumerator DeferredFrames(Action action, int frames = 1)
+    {
+        for (int i = 0; i < frames; ++i) {
+            yield return null;
+        }
+        action();
+    }
+
+    private static IEnumerator DeferredAwaiter(Action action, Func<bool> predicate)
+    {
+        yield return new WaitUntil(predicate);
+        action();
+    }
+}
